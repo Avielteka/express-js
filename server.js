4141012fc5badc51039e7fcec875cb7b2b39170e
Avielteka <aviek2260@gmail.com>
2022-11-13 13:20:17 +0200

    add

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 8080
+
+app.use(express.json({extended:true}));
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send({massage:"success"})
+})
+
+app.listen(port,()=>{
+    console.log(`you in port ${port} `);
+})
